70a669bf4a3b650e2cdae3c575abf04d93989fcf
Elius <mendinuetaj@uninorte.edu.co>
2020-05-19 00:30:48 -0500

    finalnal

--- Yacc/Prueba.sh
+++ Yacc/Prueba.sh
@@ -4,7 +4,7 @@ rm a.out &
 rm lex.yy.c &
 rm y.tab.c &
 rm y.tab.h &
-yacc -d  Comp.y &&
-flex -d Comp.l &&
+yacc -d Comp.y &&
+flex Comp.l &&
 cc lex.yy.c y.tab.c &&
 ./a.out prueba | tee salida.txt
